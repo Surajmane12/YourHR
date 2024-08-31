eb2b30485b4e4bd403b5783c211a40fdf925f390
Suraj Mane <103933043+Surajmane12@users.noreply.github.com>
2024-08-31 16:15:20 +0530

    Update corsMiddleware.js

--- Backend/middleware/corsMiddleware/corsMiddleware.js
+++ Backend/middleware/corsMiddleware/corsMiddleware.js
@@ -2,13 +2,10 @@
 const allowedOrigins = ["https://66d2f323b15368f40d37f7ff--gentle-biscotti-8619f2.netlify.app"];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*", // This allows all origins
 };
+
+module.exports = corsOptions;
+
 
 module.exports = corsOptions;
